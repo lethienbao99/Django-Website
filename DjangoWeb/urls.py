db6948e8c680935191911937c36a7da14e7caba5
lethienbao99 <1754050012bao@ou.edu.vn>
2020-08-13 14:21:48 +0700

    create movies tree

--- DjangoWeb/urls.py
+++ DjangoWeb/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.home.urls')),
+    path('movies/', include('apps.movies.urls')),
 ]
