3d157e8cf1c7a99caa9df79418126268bbfc9fbd
lethienbao99 <1754050012bao@ou.edu.vn>
2020-08-13 16:57:13 +0700

    Movie detail and Movie list

--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Movie
 
 
 def index(request):
     return render(request, 'movies/movie-list.html')
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_url_kwarg = "the_slug"
+    context_object_name = "movie"
+    slug_field = "slug"
+    template_name = "movies/movie-detail.html"
+
+
+class MovieListView(ListView):
+    model = Movie
+    queryset = Movie.objects.order_by("year")
+    context_object_name = "movie-list"
+    template_name = "movies/movie-list.html"
+    paginate_by = 10
